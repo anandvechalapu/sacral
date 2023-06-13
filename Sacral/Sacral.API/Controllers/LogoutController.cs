fe226279ff24f74f3c9f8cf7063f5732693d317e
dotnet-code <@dotnet-code>
2023-06-13 15:40:11 +0000

    commit from code202306134018

---
+++ Sacral/Sacral.API/Controllers/LogoutController.cs
@@ -0,0 +1,32 @@
+﻿using Sacral.DTO;
+using Sacral.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sacral.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly LogoutService _logoutService;
+
+        public LogoutController(LogoutService logoutService)
+        {
+            _logoutService = logoutService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] LogoutDto logoutDto)
+        {
+            var isLoggedOut = await _logoutService.LogoutAsync(logoutDto);
+            if (isLoggedOut)
+            {
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ Sacral/Sacral.DTO/LogoutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Sacral
+{
+    public class LogoutModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime LogoutTime { get; set; }
+    }
+}
